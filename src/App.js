564e8b2b2fada433632d6fd7cde7ad04520bc76c
Christian Lisangola <christian.lisangola@gmail.com>
2020-06-26 09:30:48 +0100

    compteur sans redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Compteur</h1>
+      <Compteur />
+    </>
   );
 }
 
-export default App;
+function Compteur() {
+  const [valeur, definirValeur] = useState(0);
+  return (
+    <>
+      <button onClick={() => definirValeur(valeur + 1)}>+</button>
+      <Resultat valeurActuelle={valeur} />
+      <button onClick={() => definirValeur(valeur - 1)}>-</button>
+    </>
+  );
+}
+
+function Resultat(props) {
+  return (
+    <span style={{ margin: '10px' }}>
+      La valeur est : {props.valeurActuelle}
+    </span>
+  );
+}
